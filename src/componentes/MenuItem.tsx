c4f839244711561ee863a539d1ee0395aa7421a8
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-11-08 18:27:10 -0300

    Adicionando o return do MenuItem

--- src/componentes/MenuItem.tsx
+++ src/componentes/MenuItem.tsx
@@ -5,21 +5,45 @@ interface MenuItemProps {
     icon: ReactNode;
     label: string;
     route: string;
+    className?: string;
 }
 
 export default function MenuItem({
     icon,
     label,
-    route
+    route,
+    className = ""
 }: MenuItemProps) {
     const navigate = useNavigate();
-    const handleClick = () => navigate(route);
+    function handleClick(){
+        navigate(route);
+    } 
 
+    const baseClasses = `
+        cursor-pointer
+        bg-[#F4EEE8]
+        rounded-xl
+        shadow-md
+        w-40
+        h-40
+        flex
+        flex-col
+        justify-center
+        items-center
+        transition
+        transform
+        hover:scale-105
+        hover:shadow-lg
+    `;
     return (
-        <button>
-            
+        <button
+        type="button"
+        onClick={handleClick}
+        aria-label={label}
+        className={baseClasses + " " + className}
+        >
+        <div className="text-5xl mb-3">{icon}</div>
+        <p className="font-semibold text-lg">{label}</p>
         </button>
-        
-        
-    )
+    );
 }
