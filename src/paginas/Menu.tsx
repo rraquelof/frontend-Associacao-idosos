6da375af0b3a7d953786295d7baffff21aaf6246
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-11-08 18:32:14 -0300

    Começando o Menu

--- src/paginas/Menu.tsx
+++ src/paginas/Menu.tsx
@@ -1,5 +1,9 @@
+import MenuItem from "../componentes/MenuItem";
+
 export default function Menu(){
     return(
-        <h1>Menu da página</h1>
+        <div className="min-h-screen bg-[#E7D7C9] flex flex-col items-center p-8"> 
+        <h1 className="text-3xl font-bold mb-10"> Menu Principal</h1>
+        </div>
     )
 }
