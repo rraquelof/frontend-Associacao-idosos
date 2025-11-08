28c70d81c34e74b3ec8322f9ed0c7af265949c6f
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-11-08 18:39:13 -0300

    Mudando as cores e estilo

--- src/paginas/Menu.tsx
+++ src/paginas/Menu.tsx
@@ -2,8 +2,11 @@ import MenuItem from "../componentes/MenuItem";
 
 export default function Menu(){
     return(
-        <div className="min-h-screen bg-[#E7D7C9] flex flex-col items-center p-8"> 
-        <h1 className="text-3xl font-bold mb-10"> Menu Principal</h1>
+        <div className="min-h-screen bg-white flex flex-col items-center p-8"> 
+        
+        <h1 className=" text-black font-medium text-3xl mb-10 transition-all duration-200"> 
+            Menu Principal
+            </h1>
         </div>
     )
 }
