67f3847c02ada633911a1e07a2314864c2bb7033
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-11-09 10:52:04 -0300

    Arrumando a imagem do icone de idosos do menu

--- src/componentes/MenuItem.tsx
+++ src/componentes/MenuItem.tsx
@@ -31,7 +31,6 @@ export default function MenuItem({
                     shadow-md
                     w-40
                     h-40
-                    relative
                     overflow-hidden
                     hover:scale-105
                     hover:shadow-lg
@@ -39,10 +38,7 @@ export default function MenuItem({
                     ${className}
                 `}
             >
-                <div className="absolute inset-0 bg-white flex justify-center items-center">
-                    {icon}
-                </div>
-
+                {icon}
             </button>
             <p className="mt-2 font-semibold text-lg text-black">
                 {label}
