ffcbcfa7accf56372bb6b37e417b363219b5a234
Raquel Ferreira <rraquelof@gmail.com>
2026-01-19 15:56:17 -0300

    mudando arquivos para fazer deploy

--- src/componentes/label/Label.stories.tsx
+++ src/componentes/label/Label.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Label from "../label/Label";
+import Label from "./Label";
 
 const meta: Meta<typeof Label> = {
   title: "Componentes/Label",
